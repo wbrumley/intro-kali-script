5a8ebf57566dba7559bab7854b228c493a5fc44b
Will Brumley <13753842+wbrumley@users.noreply.github.com>
2019-06-10 17:31:30 -0400

    Update MySQLscannerAdvanced.sh

--- MySQLscannerAdvanced.sh
+++ MySQLscannerAdvanced.sh
@@ -2,6 +2,9 @@
 # Small script to specify and run nmap requests 
 
 echo "Enter the starting IP address : "
+read FirstIP
+
+echo "Enter the last octet of the IP you want to scan for: "
 read LastOctectIP
 
 echo "Enter the port number you want to scan for : "
